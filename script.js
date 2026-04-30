8a07795975eca8ef33caec50579e130f258ca7a8
CoolBeansStudioOfficial <134745254+CoolBeansStudioOfficial@users.noreply.github.com>
2026-04-30 09:51:23 -0500

    added download links

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let logo = document.querySelector(".logo");
 let download = document.getElementById("download");
+let section = document.getElementById("section");
 
 logo.addEventListener("click", function () {
-    download.scrollIntoView({ behavior: "smooth", block: "start" });
+    section.scrollIntoView({ behavior: "smooth", block: "center" });
+});
+
+download.addEventListener("click", function () {
+    section.scrollIntoView({ behavior: "smooth", block: "center" });
 });
